58ead90b6bc404c49f2a6b279d0bcb93070a7081
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-09-15 20:41:49 +0200

    POpravi

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
-          <h1>Đurđice</h1>
+          <h1>Đurđice</h2>
           <div>Osijek</div>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
