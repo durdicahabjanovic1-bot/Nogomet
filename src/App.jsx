9ecc17e890e0b0a3e2d1eefaa847ff00cf0f497a
durdicahabjanovic1 <durdicahabjanovic1@gmail.com>
2026-09-15 20:28:24 +0200

    Dodavanje oznake

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <h1>Đurđice</h1>
+          <div>Osijek</div>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
